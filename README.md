540411fe3ed2c77745b4b9c0cf05a47d89733a95
GaganDeep Tomar <gagandeep280597@gmail.com>
2018-11-05 03:44:23 +0530

    structuring

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 A simple To Do App in node.js!
+Made using the standard MVC architecture.
 Using :
 - express
 - ejs
---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+todoController(app);
+
+app.listen(3000);
+console.log('Listening ...');
